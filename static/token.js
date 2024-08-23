f6f2f5d48d14b51021e4ed87480e1f0482da8b57
Elias Avalos <eavalos@request.com.ar>
2024-08-22 21:10:34 -0300

    Agregado de archivo css y sus contenedores en HTML. Se mueve funcion toggle password a archivo js

--- static/token.js
+++ static/token.js
@@ -160,3 +160,25 @@ function getAccountMovements()
         movementsErrormessage.innerText = 'No access token available.';
     }
 }
+
+// Otras funciones
+
+function togglePasswordVisibility() 
+{
+    var passwordInput = document.getElementById('private_key');
+    var eyeOpen = document.getElementById('eye-open');
+    var eyeClosed = document.getElementById('eye-closed');
+
+    if (passwordInput.type === 'password') 
+    {
+        passwordInput.type = 'text';
+        eyeOpen.style.display = 'none';
+        eyeClosed.style.display = 'inline';
+    } 
+    else 
+    {
+        passwordInput.type = 'password';
+        eyeOpen.style.display = 'inline';
+        eyeClosed.style.display = 'none';
+    }
+}
